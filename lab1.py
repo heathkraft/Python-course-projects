3df03b5a613fe1642858e8e9880e3604c15957bc
heathkraft <heathkraft@outlook.com>
2019-05-21 22:52:23 -0500

    completed first lab

---
+++ lab1.py
@@ -0,0 +1,21 @@
+#declare variables and output info based on the presented output
+name = "Fred Flintstone"
+loc = "Bedrock Area"
+relstat = "Married"
+spouse = "Wilma Flintstone"
+emplby = "Slate Rock and Gravel"
+occu = "Bronto Crane Operator"
+salary =  36000
+
+#variables declared and stored, now print the information
+print("")
+print("Name:        ", name)
+print("Location:    ", loc)
+print("")
+print("Relationship status:  ", relstat)
+print("---- Spouse:   ", spouse)
+print("")
+print("Employed By:  ", emplby)
+print("---- Occupation:     ", occu)
+print("---- Income:         ", float(salary))
+#instructor solution used monthly income, and printed salary*12 instead of float
